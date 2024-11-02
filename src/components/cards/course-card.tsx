770c234c265d63069548c6c34bbd692efde6d0fb
deviid123 <devidkoch321@gmail.com>
2024-11-02 16:43:25 +0530

    update

--- src/components/cards/course-card.tsx
+++ src/components/cards/course-card.tsx
@@ -42,7 +42,6 @@ export default function CourseCard({
 
   const courseIdsFromCookies = getCourseIdsFromCookies();
 
-
   const { data: available, mutate: priorityMutate } = useMutation({
     mutationFn: checkExistPriority,
     onSuccess(available) {
